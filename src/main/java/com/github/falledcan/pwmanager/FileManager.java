90df395fb58088d9c1ecf0b6414363683ac8ed8b
FalledCan <nagikun08@gmail.com>
2024-06-22 19:09:01 +0900

    0.0.2

--- src/main/java/com/github/falledcan/pwmanager/FileManager.java
+++ src/main/java/com/github/falledcan/pwmanager/FileManager.java
@@ -1,31 +1,27 @@
 package com.github.falledcan.pwmanager;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Base64;
 
-public class Test {
-    public static void main(String[] args) {
+public class FileManager {
+
+    public static void createFiles() {
         // ユーザーのローカルアプリケーションデータディレクトリを取得
         String userHome = System.getProperty("user.home");
         Path localAppDataPath = Paths.get(userHome, "AppData", "Local", "PasswordManager");
-
         try {
             // ディレクトリが存在しない場合は作成
             if (!Files.exists(localAppDataPath)) {
                 Files.createDirectories(localAppDataPath);
             }
-
+            //データベースファイル作成
             Path filePath = localAppDataPath.resolve("pw.db");
             if(!Files.exists(filePath)){
                 Files.createFile(filePath);
             }
+            //暗号キーファイル作成
             filePath = localAppDataPath.resolve("key.json");
             if(!Files.exists(filePath)){
                 Files.createFile(filePath);
