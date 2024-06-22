062a74ae5f883d168e1f5d6a128c9cf10a6e7919
FalledCan <nagikun08@gmail.com>
2024-06-22 16:42:02 +0900

    0.0.1 - Test

---
+++ src/main/java/com/github/falledcan/pwmanager/Encryption.java
@@ -0,0 +1,63 @@
+package com.github.falledcan.pwmanager;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class Encryption {
+    private static final String ALGORITHM = "AES";
+    private static final int KEY_SIZE = 256; // AES-256
+    private static String key;
+
+    public static void main(String[] args) throws Exception {
+        // ランダムなAES鍵を生成
+        String aesKey = generateRandomAESKey();
+        System.out.println("Generated AES Key: " + aesKey);
+
+        // 暗号化する文字列
+        String originalString = "たしかにそうかも知れないですね";
+        System.out.println("Original String: " + originalString);
+
+        // 暗号化
+        String encryptedString = encrypt(originalString, aesKey);
+        System.out.println("Encrypted String: " + encryptedString);
+
+        // 復号化
+        String decryptedString = decrypt(encryptedString, aesKey);
+        System.out.println("Decrypted String: " + decryptedString);
+    }
+
+    //指定されたデータをAESで暗号化
+    public static String encrypt(String data, String key) throws Exception {
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        SecretKeySpec secretKey = new SecretKeySpec(decodedKey, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    // 指定された暗号化データをAESで復号化
+    public static String decrypt(String encryptedData, String key) throws Exception {
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        SecretKeySpec secretKey = new SecretKeySpec(decodedKey, ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(decryptedData);
+    }
+
+    //ランダムなAES鍵を生成
+    public static String generateRandomAESKey() throws Exception {
+
+            KeyGenerator keyGen = KeyGenerator.getInstance(ALGORITHM);
+            keyGen.init(KEY_SIZE); // AES-256ビットの鍵を生成
+            SecretKey secretKey = keyGen.generateKey();
+            return key = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+
+    }
+
+}
+
---
+++ src/main/java/com/github/falledcan/pwmanager/Test.java
@@ -0,0 +1,11 @@
+package com.github.falledcan.pwmanager;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class Test {
+
+}
