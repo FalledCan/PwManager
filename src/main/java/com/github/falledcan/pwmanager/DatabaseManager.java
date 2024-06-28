7a842c2bfcb661a69223857b865c807532cbfd47
FalledCan <nagikun08@gmail.com>
2024-06-28 13:53:27 +0900

    0.0.5 DB

--- src/main/java/com/github/falledcan/pwmanager/DatabaseManager.java
+++ src/main/java/com/github/falledcan/pwmanager/DatabaseManager.java
@@ -1,16 +1,15 @@
 package com.github.falledcan.pwmanager;
 
+import javax.swing.plaf.nimbus.State;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseManager {
 
     private  Connection conn = null;
 
+    //データベースロード
     public void loadDB(){
 
         String userHome = System.getProperty("user.home");
@@ -24,8 +23,37 @@ public class DatabaseManager {
         }
     }
 
+    //テーブル作成
+    public void createTable() throws SQLException {
 
+        String sql = "create table if not exists list(id integer primary key,name not NULL,url text,password text not NULL,memo text);";
+        Statement stmt = conn.createStatement();
+
+        stmt.execute(sql);
+    }
+
+    //データの挿入
+    public void insertData(String name, String url, String password, String memo) throws SQLException {
+
+        String sql = "insert into list(name, url, password, memo) values(?,?,?,?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1,name);
+        pstmt.setString(2,url);
+        pstmt.setString(3,password);
+        pstmt.setString(4,memo);
+
+        pstmt.executeUpdate();
+    }
+
+    //データの更新
+    public void updataData(){
+
+    }
+
+    //データベースを閉じる
     public void close() throws SQLException {
+
         if(conn != null){
             conn.close();
         }
