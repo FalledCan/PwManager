e183bb932b90bc9b5c7ff95d47d6d59eed521338
FalledCan <nagikun08@gmail.com>
2024-07-01 15:12:09 +0900

    0.0.7

--- src/main/java/com/github/falledcan/pwmanager/DatabaseManager.java
+++ src/main/java/com/github/falledcan/pwmanager/DatabaseManager.java
@@ -25,50 +25,63 @@ public class DatabaseManager {
 
     //テーブル作成
     public void createTable() throws SQLException {
-        String sql = "create table if not exists list(id integer primary key,name not NULL,url text,password text not NULL,memo text);";
+        String sql = "create table if not exists list(id integer primary key,name not NULL,"
+                + "url text,username text not NULL,password text not NULL,memo text);";
         Statement stmt = conn.createStatement();
 
         stmt.execute(sql);
     }
 
     //データの挿入
-    public void insertData(String name, String url, String password, String memo) throws SQLException {
+    public void insertData(String name, String url,String userName, String password, String memo) throws SQLException {
 
-        String sql = "insert into list(name, url, password, memo) values(?,?,?,?)";
+        String sql = "insert into list(name, url, username, password, memo) values(?,?,?,?,?)";
         PreparedStatement pstmt = conn.prepareStatement(sql);
 
         pstmt.setString(1,name);
         pstmt.setString(2,url);
-        pstmt.setString(3,password);
-        pstmt.setString(4,memo);
+        pstmt.setString(3,userName);
+        pstmt.setString(4,password);
+        pstmt.setString(5,memo);
 
         pstmt.executeUpdate();
     }
 
     //データの更新
-    public void updataData(int id, String nName, String nUrl, String nPassword, String nMemo) throws SQLException {
+    public void updataData(int id, String nName, String nUrl,String nUserName, String nPassword, String nMemo) throws SQLException {
 
-        String sql = "update list set name = ?, url = ?, password = ?, memo = ? WHERE id = ?";
+        String sql = "update list set name = ?, url = ?, username = ?, password = ?, memo = ? WHERE id = ?";
         PreparedStatement pstmt = conn.prepareStatement(sql);
 
         pstmt.setString(1,nName);
         pstmt.setString(2,nUrl);
-        pstmt.setString(3,nPassword);
-        pstmt.setString(4,nMemo);
-        pstmt.setInt(5,id);
+        pstmt.setString(3,nUserName);
+        pstmt.setString(4,nPassword);
+        pstmt.setString(5,nMemo);
+        pstmt.setInt(6,id);
 
         pstmt.executeUpdate();
 
     }
 
     //データの削除
-    public void deleteDAte(int id) throws SQLException {
+    public void deleteDate(int id) throws SQLException {
         String sql = "delete from list where id = ?";
         PreparedStatement pstmt = conn.prepareStatement(sql);
 
         pstmt.setInt(1,id);
 
         pstmt.executeUpdate();
+    }
+
+    //データの取得
+    public void getData(int id){
+
+    }
+
+    //データサーチ
+    public void searchData(String name){
+
     }
 
     //データベースを閉じる
