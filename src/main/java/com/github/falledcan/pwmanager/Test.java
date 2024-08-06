417da37eebfb034caa9f48ad792ae8688bebb08c
FalledCan <nagikun08@gmail.com>
2024-08-06 21:49:02 +0900

    0.0.13 testDataBese

--- src/main/java/com/github/falledcan/pwmanager/Test.java
+++ src/main/java/com/github/falledcan/pwmanager/Test.java
@@ -1,6 +1,7 @@
 package com.github.falledcan.pwmanager;
 
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Test {
 
@@ -8,6 +9,8 @@ public class Test {
         fast_load();
 
         System.out.println(Encryption.isCheckDataBase());
+        //System.out.println(DatabaseManager.insertData("aaa",null,"Beercna","adfadsdf",null));
+        //System.out.println(DatabaseManager.deleteDate(10));
     }
 
     //初期データロード
@@ -20,7 +23,25 @@ public class Test {
         //暗号化キーロード
         Encryption.loadKey();
 
+        if(!Encryption.isCheckDataBase()){
+            System.exit(0);
+        }
 
+        int Row_count = DatabaseManager.getRowCount();
+        if(Row_count != 0){
+
+            ArrayList<String[]> list = DatabaseManager.getAllData();
+            for(String[] datas: list){
+                System.out.println(datas[0]);
+                System.out.println(datas[1]);
+                System.out.println(datas[2]);
+                System.out.println(datas[3]);
+                System.out.println(datas[4]);
+
+            }
+
+        }
+        System.out.println(Row_count);
     }
 
 }
