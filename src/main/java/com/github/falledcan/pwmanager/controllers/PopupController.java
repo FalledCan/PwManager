2b2759e0887cee2c8aff219227901756d536b043
FalledCan <nagikun08@gmail.com>
2024-08-11 20:49:48 +0900

    0.0.21

--- src/main/java/com/github/falledcan/pwmanager/controllers/PopupController.java
+++ src/main/java/com/github/falledcan/pwmanager/controllers/PopupController.java
@@ -3,6 +3,7 @@ package com.github.falledcan.pwmanager.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class PopupController {
@@ -13,14 +14,33 @@ public class PopupController {
     @FXML
     private Button popbutton;
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @FXML
+    private HBox titleBar;
+
     @FXML
     public void initialize() {
         poptext.setText("登録しました。");
+        makeStageDraggable();
     }
 
     @FXML
     private void closeButton() {
         Stage stage = (Stage) popbutton.getScene().getWindow();
         stage.close();
+    }
+    //カーソルで移動
+    private void makeStageDraggable() {
+        titleBar.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        titleBar.setOnMouseDragged(event -> {
+            Stage stage = (Stage) titleBar.getScene().getWindow();
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
     }
 }
